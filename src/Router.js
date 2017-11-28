6a0137acb7eabb9c434c7ddd5ddf6d919a0db53c
nathanharig <nathanharig@gmail.com>
2017-11-28 11:59:15 -0500

    Added employee list

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Scene, Router, Stack } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
   return (
-    <Router style= {{ paddingTop: 10 }}>
+    <Router sceneStyle={{ paddingTop: 10 }}>
       <Stack key="root">
       <Scene key="login" component={LoginForm} title="Please Login" />
+      <Scene key="employeeList" component={EmployeeList} title="Employees" initial />
       </Stack>
     </Router>
   );
 };
-
-
 
 export default RouterComponent;
