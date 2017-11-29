7a5c17f74c42933e1d9074ccebaeae7281510a1a
nathanharig <nathanharig@gmail.com>
2017-11-29 14:04:23 -0500

    Final Product

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -3,7 +3,9 @@ import { Actions } from 'react-native-router-flux';
 import {
   EMPLOYEE_UPDATE,
   EMPLOYEE_CREATE,
-  EMPLOYEES_FETCH_SUCCESS
+  EMPLOYEES_FETCH_SUCCESS,
+  EMPLOYEE_SAVE_SUCCESS,
+  EMPLOYEE_DELETE_SUCCESS
 } from './types';
 
 
@@ -35,6 +37,32 @@ export const employeesFetch = () => {
     firebase.database().ref(`users/${currentUser.uid}/employees`)
     .on('value', snapshot => {
       dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
+    });
+  };
+};
+
+export const employeeSave = ({ name, phone, shift, uid }) => {
+  const { currentUser } = firebase.auth();
+
+  return (dispatch) => {
+  firebase.database().ref(`users/${currentUser.uid}/employees/${uid}`)
+    .set({ name, phone, shift })
+    .then(() => {
+      dispatch({ type: EMPLOYEE_SAVE_SUCCESS });
+      Actions.pop();
+    });
+  };
+};
+
+export const employeeDelete = ({ uid }) => {
+  const { currentUser } = firebase.auth();
+
+  return (dispatch) => {
+    firebase.database().ref(`users/${currentUser.uid}/employees/${uid}`)
+    .remove()
+    .then(() => {
+      dispatch({ type: EMPLOYEE_DELETE_SUCCESS });
+      Actions.pop();
     });
   };
 };
--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,6 +1,8 @@
 import {
   EMPLOYEE_UPDATE,
-  EMPLOYEE_CREATE
+  EMPLOYEE_CREATE,
+  EMPLOYEE_SAVE_SUCCESS,
+  EMPLOYEE_DELETE_SUCCESS
 } from '../actions/types';
 
 const INITIAL_STATE = {
@@ -15,6 +17,10 @@ export default (state = INITIAL_STATE, action) => {
       //action.payload === { prop: 'name', value: 'jane' }
       return { ...state, [action.payload.prop]: action.payload.value };
     case EMPLOYEE_CREATE:
+      return INITIAL_STATE;
+    case EMPLOYEE_SAVE_SUCCESS:
+      return INITIAL_STATE;
+    case EMPLOYEE_DELETE_SUCCESS:
       return INITIAL_STATE;
     default:
     return state;
